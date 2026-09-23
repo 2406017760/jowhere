78d5a8b62b1468af819d7b18c62b63ca7b37ee15
Joey <2406017760@qq.com>
2026-09-23 10:53:09 +0800

    [feat] adapt cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://example.com',
+  adapter: cloudflare(),
   integrations: [sitemap()],
 });
